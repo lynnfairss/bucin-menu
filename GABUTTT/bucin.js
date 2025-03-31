8b066b050b2a0911ff1631595c6b8f4e126892d1
lynnfairss <lyanfairusathallah32@gmail.com>
2025-03-31 19:52:49 +0700

    Delete GABUTTT directory

--- GABUTTT/bucin.js
+++
@@ -1,9 +0,0 @@
-function showMenu() {
-    document.getElementById('menu').style.display = 'block';
-}
-
-function showContent(id) {
-    let contents = document.querySelectorAll('.content');
-    contents.forEach(content => content.style.display = 'none');
-    document.getElementById(id).style.display = 'block';
-}
